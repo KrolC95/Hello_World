1fca0ce09ad414fd4ece9f080c9322bd2504025f
Cezary <90151509+KrolC95@users.noreply.github.com>
2022-02-27 22:51:56 +0100

    my first repository

---
+++ hello_world_2.py
@@ -0,0 +1,8 @@
+print('Hello Python on this computer')
+imie = input('Enter your first name: ')
+nazwisko = input('Enter your second name ')
+print('Hi', imie, nazwisko)
+print('Nice to see you!')
+input('Press enter to continue...')
+
+# print(type(imie),type(nazwisko))
